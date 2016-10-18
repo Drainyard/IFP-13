4dfe7f77021b6c68e966c050ba961d65b191cee6
marit-edna <marit-o@gmx.de>
2016-10-18 18:48:29 +0200

    Done for today

--- exam/fib.scm
+++ exam/fib.scm
@@ -112,7 +112,7 @@
              1]
             [else
              (let-values ([(r2 r1) (visit n_init)])
-               (+ r1 r2))])
+               (+ r2 r1))])
           (errorf 'fib_lin2
                   "Not a non-negative integer: ~s"
                   n_init)))))
@@ -403,6 +403,15 @@
                 n_init)))))
 
 
+;;; This version also has logarithmic time complexity, since the input argument
+;;; is roughly halved for each call. The fact that there are two or three 
+;;; different calls only gives a constant factor and therefore does not affect 
+;;; the overall complexity.
+
+(unless (test-fib fib_p-q)
+  (printf "fail: (test-fib fib_p-q) ~n"))
+
+
 ;;;;;;;;;;
 
 (define power_Magritte
@@ -447,6 +456,222 @@
                                     `(* (square ,(visit (quotient i 2))) ,base))))])
             (visit n))))))
 
+
+
+;;; First, a magrittified version of the naive fib function given to us:
+(define test-fib_v0_Magritte
+  (lambda (candidate)
+    (and (equal? (candidate 1)
+                 '1)
+         (equal? (candidate 2)
+                 '(+ 1 0))
+         (equal? (candidate 3)
+                 '(+ (+ 1 0) 1))
+         (equal? (candidate 4)
+                 '(+ (+ (+ 1 0) 1) (+ 1 0)))
+         ; etc
+         )))
+
+
+(define fib_v0_Magritte
+  (lambda (n_init)
+    (letrec ([visit (lambda (n)
+                      (cond
+                        [(= n 0)
+                         `0]
+                        [(= n 1)
+                         `1]
+                        [else
+                         `(+ ,(visit (- n 1)) ,(visit (- n 2)))]))])
+      (if (and (integer? n_init)
+               (not (negative? n_init)))
+          `,(visit n_init)
+          (errorf 'fib_v0
+                  "Not a non-negative integer: ~s"
+                  n_init)))))
+
+(unless (test-fib_v0_Magritte fib_v0_Magritte)
+  (printf "fail: (test-fib_v0_Magritte fib_v0_Magritte) ~n"))
+
+
+;;; The first linear version
+(define test-fib_lin_Magritte
+  (lambda (candidate)
+    (and (equal? (candidate 1)
+                 '1)
+         (equal? (candidate 2)
+                 '(+ 0 1))
+         (equal? (candidate 3)
+                 '(+ 1 1))
+         (equal? (candidate 4)
+                 '(+ 1 2))
+         (equal? (candidate 5)
+                 '(+ 2 3))
+         ; etc
+         )))
+
+(define fib_lin1_Magritte
+  (lambda (n_init)
+    (letrec ([visit (lambda (n a1 a2)
+                      (cond
+                        [(= n 2)
+                         `(+ ,a1 ,a2)]
+                        [else
+                         (visit (- n 1) a2 (+ a1 a2))]))])
+      (if (and (integer? n_init)
+               (not (negative? n_init)))
+          (cond 
+            [(= n_init 0)
+             `0]
+            [(= n_init 1)
+             `1]
+            [else
+             `,(visit n_init 0 1)])
+          (errorf 'fib_lin1_Magritte
+                  "Not a non-negative integer: ~s"
+                  n_init)))))
+
+(unless (test-fib_lin_Magritte fib_lin1_Magritte)
+  (printf "fail: (test-fib_lin_Magritte fib_lin1_Magritte) ~n"))
+
+;;; The second linear version can use the same test case as the first:
+
+(define fib_lin2_Magritte
+  (lambda (n_init)
+    (letrec ([visit (lambda (n)
+                      (cond
+                        [(= n 2)
+                         (values 0 1)]
+                        [else
+                         (let-values ([(f2 f1) (visit (- n 1))])
+                           (values f1 (+ f2 f1)))]))])
+      (if (and (integer? n_init)
+               (not (negative? n_init)))
+          (cond 
+            [(= n_init 0)
+             `0]
+            [(= n_init 1)
+             `1]
+            [else
+             (let-values ([(r2 r1) (visit n_init)])
+               `(+ ,r2 ,r1))])
+          (errorf 'fib_lin2_Magritte
+                  "Not a non-negative integer: ~s"
+                  n_init)))))
+
+(unless (test-fib_lin_Magritte fib_lin2_Magritte)
+  (printf "fail: (test-fib_lin_Magritte fib_lin2_Magritte) ~n"))
+
+;;; fib using matrices:
+(define test-fib_matrix_naive_Magritte
+  (lambda (candidate)
+    (and (equal? (candidate 1)
+                 '1)
+         (equal? (candidate 2)
+                 '(x11 (matrix-exp_naive fib-base-matrix 1)))
+         (equal? (candidate 7)
+                 '(x11 (matrix-exp_naive fib-base-matrix 6)))
+         ; etc
+         )))
+
+
+(define fib_matrix_naive_Magritte
+  (lambda (n)
+    (if (and (integer? n)
+             (not (negative? n)))
+        (cond
+          [(= n 0)
+           `0]
+          [(= n 1)
+           `1]
+          [else
+           `(x11 (matrix-exp_naive fib-base-matrix ,(1- n)))])
+        (errorf 'fib_matrix_naive_Magritte
+                "Not a non-negative integer: ~s"
+                n))))
+
+(unless (test-fib_matrix_naive_Magritte fib_matrix_naive_Magritte)
+  (printf "fail: (test-fib_matrix_naive_Magritte fib_matrix_naive_Magritte) ~n"))
+
+
+(define test-fib_matrix_Magritte
+  (lambda (candidate)
+    (and (equal? (candidate 1)
+                 '1)
+         (equal? (candidate 2)
+                 '(x11 (matrix-exp fib-base-matrix 1)))
+         (equal? (candidate 7)
+                 '(x11 (matrix-exp fib-base-matrix 6)))
+         ; etc
+         )))
+
+
+(define fib_matrix_Magritte
+  (lambda (n)
+    (if (and (integer? n)
+             (not (negative? n)))
+        (cond
+          [(= n 0)
+           `0]
+          [(= n 1)
+           `1]
+          [else
+           `(x11 (matrix-exp fib-base-matrix ,(1- n)))])
+        (errorf 'fib_matrix_Magritte
+                "Not a non-negative integer: ~s"
+                n))))
+
+
+(unless (test-fib_matrix_Magritte fib_matrix_Magritte)
+  (printf "fail: (test-fib_matrix_Magritte fib_matrix_Magritte) ~n"))
+
+;;; Last but not least, fib with the p-q property:
+(define test-fib_p-q_Magritte
+  (lambda (candidate)
+    (and (equal? (candidate 1)
+                 '1)
+         (equal? (candidate 2)
+                 '1)
+         (equal? (candidate 3)
+                 '(+ (square 1) (square 1)))
+         (equal? (candidate 4)
+                 '(+ (* 1 (+ (square 1) (square 1)))
+                                (* 1 1)))
+         (equal? (candidate 5)
+                 '(+ (square (+ (square 1) (square 1)))
+                                (square 1)))
+         ; etc
+         )))
+
+(define fib_p-q_Magritte
+  (lambda (n_init)
+    (letrec ([visit (lambda (i)
+                      (if (even? i)
+                          (cond [(= i 0)
+                                 `0]
+                                [(= i 2)
+                                 `1]
+                                [else (let* ([p (1- (quotient i 2))]
+                                             [fibp+1 (visit (1+ p))])
+                                        `(+ (* ,fibp+1 ,(visit (+ p 2)))
+                                            (* ,fibp+1 ,(visit p))))])
+                          (if (= i 1)
+                              `1
+                              (let* ([p (quotient (1- i) 2)]
+                                     [fibp+1 (visit (1+ p))]
+                                     [fibp (visit p)])
+                                `(+ (square ,fibp+1)
+                                    (square ,fibp))))))])
+    (if (and (integer? n_init)
+             (not (negative? n_init)))
+        `,(visit n_init)
+        (errorf 'fib_p-q_Magritte
+                "Not a non-negative integer: ~s"
+                n_init)))))
+
+(unless (test-fib_p-q_Magritte fib_p-q_Magritte)
+  (printf "fail: (test-fib_p-q_Magritte fib_p-q_Magritte) ~n"))
+
 ;;;;;;;;;;
 
 (define about-fib
@@ -480,6 +705,9 @@
 (unless (test-about-fib fib_matrix)
   (printf "fail: (test-about-fib fib_matrix) ~n"))
 
+(unless (test-about-fib fib_p-q)
+  (printf "fail: (test-about-fib fib_p-q) ~n"))
+
 ;;;;;;;;;;
 
 (define test-index-of-nearest-fibonacci-number
@@ -505,6 +733,50 @@
          (= (candidate 23) 9)   ;;; since fib(8) = 21 < 23 <= 34 = fib(9)
          ;;; etc.
          )))
+
+
+(define index-of-nearest-fibonacci-number_ugly
+  (lambda (n)
+    (letrec ([visit 
+              (lambda (i)
+                (if (< (fib_p-q i) n)
+                    (visit (1+ i))
+                    i))])
+      (if (and (number? n)
+               (not (negative? n)))
+          (visit 0)
+          (errorf 'index-of-nearest-fibonacci-number_ugly
+                  "Not a non-negative integer: ~s"
+                  n)))))
+                    
+;;; This is a very naive solution, that calls fib for every natural number until
+;;; the result is greater than or equal to the input.
+
+(unless (test-index-of-nearest-fibonacci-number index-of-nearest-fibonacci-number_ugly)
+  (printf "fail: (test-index-of-nearest-fibonacci-number index-of-nearest-fibonacci-number_ugly) ~n"))
+
+
+(define index-of-nearest-fibonacci-number_lin1
+  (lambda (n)
+    (letrec ([visit (lambda (i a1 a2)
+                      (if (< (+ a1 a2) n)
+                          (visit (1+ i) a2 (+ a1 a2))
+                          i))])
+      (if (and (number? n)
+               (not (negative? n)))
+          (cond [(= 0 n)
+                 0]
+                [(= 1 n)
+                 1]
+                [else
+                 (visit 3 1 1)])
+          (errorf 'index-of-nearest-fibonacci-number_lin1
+                  "Not a non-negative integer: ~s"
+                  n)))))
+
+
+(unless (test-index-of-nearest-fibonacci-number index-of-nearest-fibonacci-number_lin1)
+  (printf "fail: (test-index-of-nearest-fibonacci-number index-of-nearest-fibonacci-number_lin1) ~n"))
 
 ;;;;;;;;;;;;;;;;;;;;
 
